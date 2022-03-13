631e529ca48f7319f95f423c6a3fd3fe73484f05
Pawan <airs4u@gmail.com>
2022-03-14 00:31:34 +0530

    exam data assignments

---
+++ doors/thief.scala
@@ -0,0 +1,13 @@
+package com.training.doors
+
+object thief {
+  def main(args: Array[String]): Unit = {
+
+    val lines = io.Source.fromFile("C:\\Work\\Training1\\exam_data.txt").getLines
+    lines.foreach(
+      {l => val col = l.split(",")
+        if (col(0).toDouble * col(1).toDouble <= col(2).toDouble) println("Yes"+ col(0)) else println("No"+col(0))
+    })
+
+  }
+}
